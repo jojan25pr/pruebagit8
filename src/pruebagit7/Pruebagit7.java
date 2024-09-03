f3897347be2606434f4450904f917f0109ab6856
jojan25pr <jojan.chatgpt@gmail.com>
2024-09-03 14:46:46 -0500

    primer commit

---
+++ src/pruebagit7/Pruebagit7.java
@@ -0,0 +1,11 @@
+
+package pruebagit7;
+
+public class Pruebagit7 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Primer texto");
+    }
+    
+}
