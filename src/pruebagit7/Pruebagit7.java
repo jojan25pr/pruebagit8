6593cec214453130a73a5b6614a7ec8652ea9773
jojan25pr <jojan.chatgpt@gmail.com>
2024-09-03 14:51:10 -0500

    Segundo commit

--- src/pruebagit7/Pruebagit7.java
+++ src/pruebagit7/Pruebagit7.java
@@ -6,6 +6,8 @@ public class Pruebagit7 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Primer texto");
+        
+        System.out.println("Segundo texto, segundo commit");
     }
     
 }
